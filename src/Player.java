6e7b42b3af34e7fbc9c0189f35b297605ac789c0
HarutyunyanLiana <liana_harutyunyan@edu.aua.am>
2018-12-11 00:08:27 +0400

    Add files via upload

--- src/Player.java
+++ src/Player.java
@@ -7,6 +7,7 @@ public class Player {
     private boolean completed;
     private int notFoundBombs;
     private Game game;
+    public int success = 0;
 
     public Player(Game game) {
         this.board = new char[game.getBoardWidth()][game.getBoardHeight()];
@@ -27,17 +28,26 @@ public class Player {
         return board[x][y] == '#';
     }
 
-    private void markFlag(Coordinate coords) {
-        if (board[coords.x][coords.y] != '#') {
-            board[coords.x][coords.y] = '#';
+    private boolean isOpenedNumber(int x, int y) {
+        return (!isNotVisible(x, y) && !isFlag(x, y));
+    }
+
+    private void updateBoard(Coordinate coord, char to) {
+        board[coord.x][coord.y] = to;
+    }
+
+    private void markFlag(Coordinate coord) {
+        if (board[coord.x][coord.y] != '#') {
+            board[coord.x][coord.y] = '#';
             notFoundBombs--;
-            updateAfterFlag(coords);
+            updateAfterFlag(coord);
+            print();
         }
     }
 
-    private void getResultsOfClick(Coordinate coords) {
+    private void getResultsOfClick(Coordinate coord) {
         if (!completed) {
-            if (game.isBomb(coords)) {
+            if (game.isBomb(coord)) {
                 for (char[] c : board) {
                     Arrays.fill(c, 'X');
                 }
@@ -45,48 +55,26 @@ public class Player {
                 print();
                 System.out.println("You lost the game! Try Again");
                 return;
-            } else if (game.isNumber(coords)) {
-                updateBoard(coords, game.getNumber(coords));
-                print();
+            } else if (game.isNumber(coord)) {
+                updateBoard(coord, game.getNumber(coord));
             } else {
-                //start = true;
-                openZeros(coords);
-                print();
+                openZeros(coord);
                 coverTrivialCases();
+                print();
             }
             coverTrivialCases();
-
         }
     }
 
-    public void play() {
-        while (!completed) {
-            Random r = new Random();
-            int r_row = r.nextInt(board.length);
-            int r_col = r.nextInt(board[0].length);
 
-            if (isNotVisible(r_row, r_col)) {
-                Coordinate coords = new Coordinate(r_row, r_col);
-                coords.print();
-                getResultsOfClick(coords);
-            }
-        }
-    }
-
-    public void updateBoard(Coordinate coords, char to) {
-        board[coords.x][coords.y] = to;
-    }
-
-
-
-    public void openZeros(Coordinate coords) {
-        if (game.isZero(coords)) {
-            updateBoard(coords, '0');
+    private void openZeros(Coordinate coord) {
+        if (game.isZero(coord)) {
+            updateBoard(coord, '0');
             for (int x = -1; x < 2; ++x) {
                 for (int y = -1; y < 2; ++y) {
                     try {
-                        if (isNotVisible(coords.x + x, coords.y + y)) {
-                            openZeros(new Coordinate(coords.x + x, coords.y + y));
+                        if (isNotVisible(coord.x + x, coord.y + y)) {
+                            openZeros(new Coordinate(coord.x + x, coord.y + y));
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
                         continue;
@@ -95,65 +83,51 @@ public class Player {
 
             }
         } else {
-            updateBoard(coords, game.getNumber(coords));
+            updateBoard(coord, game.getNumber(coord));
         }
     }
 
-    public void coverTrivialCases() {
+
+    private void deactivateCertainBombs(int i, int j) {
+        ArrayList<Coordinate> CoordinateList = new ArrayList<>();
+        int bombCount = 0;
+        for (int x = -1; x < 2; ++x) {
+            for (int y = -1; y < 2; ++y) {
+                try {
+                    if (isNotVisible(i + x, j + y) || isFlag(i + x, j + y)) {
+                        bombCount++;
+                        if (!isFlag(i + x, j + y)) {
+                            CoordinateList.add(new Coordinate(i + x, j + y));
+                        }
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+        }
+
+        if (bombCount == Character.getNumericValue(game.getNumber(new Coordinate(i, j)))) {
+            for (Coordinate c : CoordinateList) {
+                markFlag(c);
+            }
+        }
+    }
+
+    private void coverTrivialCases() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (game.isNumber(new Coordinate(i, j))) {
-                    ArrayList<Coordinate> CoordinateList = new ArrayList<>();
-                    int bombCount = 0;
-                    for (int x = -1; x < 2; ++x) {
-                        for (int y = -1; y < 2; ++y) {
-                            try {
-                                if ((isNotVisible(i + x, j + y) || isFlag(i + x, j + y))
-                                        && (x != 0 || y != 0)) {
-                                    bombCount++;
-                                    if (isNotVisible(i + x, j + y) && !isFlag(i + x, j + y)) {
-                                        CoordinateList.add(new Coordinate(i + x, j + y));
-                                    }
-                                }
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                                continue;
-                            }
-                        }
-                    }
-
-                    if (bombCount == Character.getNumericValue(game.getNumber(new Coordinate(i, j)))) {
-                        for (Coordinate c : CoordinateList) {
-                            markFlag(c);
-                        }
-                    }
+                if (isOpenedNumber(i, j)) {
+                    deactivateCertainBombs(i, j);
                 }
             }
         }
-        print();
-        System.out.println();
+
     }
 
 
-    public void print() {
-        if (notFoundBombs == 0) {
-            completed = true;
-            game.printSolution();
-            System.out.println("You won the game!");
-        } else {
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    System.out.print(board[i][j] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println("Remaining bombs to find are " + notFoundBombs);
-        }
-    }
-
-
-    private void updateAfterFlag(Coordinate coords) {
-        int bombX = coords.x;
-        int bombY = coords.y;
+    private void updateAfterFlag(Coordinate bomb) {
+        int bombX = bomb.x;
+        int bombY = bomb.y;
         for (int x = -1; x < 2; ++x) {
             for (int y = -1; y < 2; ++y) {
                 try {
@@ -166,21 +140,162 @@ public class Player {
     }
 
     private void doubleClick(int x, int y) {
-        ArrayList<Coordinate> CoordinateList = new ArrayList<Coordinate>();
+        ArrayList<Coordinate> CoordinateList = new ArrayList<>();
         int count = 0;
         for (int i = -1; i < 2; ++i) {
             for (int j = -1; j < 2; ++j) {
-                if (isFlag(i + x, j + y)) {
-                    count++;
-                } else if (isNotVisible(i+x, j+y)) {
-                    CoordinateList.add(new Coordinate(i + x, j + y));
+                try {
+                    if (isFlag(i + x, j + y)) {
+                        count++;
+                    } else if (isNotVisible(i + x, j + y)) {
+                        CoordinateList.add(new Coordinate(i + x, j + y));
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue;
                 }
             }
         }
         if (count ==  Character.getNumericValue(game.getNumber(new Coordinate(x, y)))) {
             for (Coordinate c : CoordinateList) {
                 getResultsOfClick(c);
+//                c.print();
+                if (!completed) {
+                    doubleClick(c.x, c.y);
+                }
             }
+        }
+    }
+
+    public void clickRandom() {
+        Random r = new Random();
+        int r_row = r.nextInt( board.length);
+        int r_col = r.nextInt(board[0].length);
+
+        if (isNotVisible(r_row, r_col)) {
+            Coordinate coord = new Coordinate(r_row, r_col);
+            getResultsOfClick(coord);
+        }
+    }
+
+    public void findPatterns() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (isNotVisible(i, j)) {
+                    for (int x = -1; x < 2; x += 2) {
+                        try {
+                            if (isOpenedNumber(i + x, j)) {
+                                checkPatternRightLeft(i, j, x);
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            continue;
+                        }
+                    }
+
+                    for (int x = -1; x < 2; x += 2) {
+                        try {
+                            if (isOpenedNumber(i, j+x)) {
+                                checkPatternUpDown(i, j, x);
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            continue;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void checkPatternRightLeft(int i, int j, int step) {
+        int count = 0;
+        int[] patterns = new int[4];
+        for (int x = 0; x < 4; ++x) {
+            try {
+                if (isOpenedNumber(i, j+x) && isNotVisible(i-step, j+x)) {
+                    patterns[x] = realValueOfCell(i, j + x);
+                    count++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                continue;
+            }
+        }
+        if (count == 3 && patterns[0] == 1 && patterns[1] == 2 && patterns[2] == 1) {
+            markFlag(new Coordinate(i-step, j));
+            markFlag(new Coordinate(i-step, j+2));
+        } else if (count == 4 && patterns[0] == 1 && patterns[1] == 2 && patterns[2] == 2 && patterns[3] == 1) {
+            markFlag(new Coordinate(i-step, j+1));
+            markFlag(new Coordinate(i-step, j+2));
+        }
+    }
+
+    public void checkPatternUpDown(int i, int j, int step) {
+        int count = 0;
+        int[] patterns = new int[4];
+        for (int x = 0; x < 4; ++x) {
+            try {
+                if (isOpenedNumber(i + x, j) && isNotVisible(i+x, j-step)) {
+                    patterns[x] = realValueOfCell(i + x, j);
+                    count++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                continue;
+            }
+        }
+        if (count == 3 && patterns[0] == 1 && patterns[1] == 2 && patterns[2] == 1) {
+            markFlag(new Coordinate(i, j - step));
+            markFlag(new Coordinate(i+2, j - step));
+        } else if (count == 4 && patterns[0] == 1 && patterns[1] == 2 && patterns[2] == 2 && patterns[3] == 1) {
+            markFlag(new Coordinate(i+1, j - step));
+            markFlag(new Coordinate(i+2, j - step));
+        }
+    }
+
+    public int realValueOfCell(int i, int j) {
+        int currentValue = Character.getNumericValue(game.getNumber(new Coordinate(i, j)));
+        for (int x = -1; x < 2; ++x) {
+            for (int y = -1; y < 2; ++y) {
+                try {
+                    if (isFlag(i + x, j + y)) {
+                        currentValue--;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue;
+                }
+
+            }
+        }
+        return currentValue;
+    }
+
+
+
+
+    public void play() {
+        clickRandom();
+        while (notFoundBombs != 0 && !completed) {
+            findPatterns();
+            clickRandom();
+        }
+        if (notFoundBombs == 0) {
+            completed = true;
+            success = 1;
+            game.printSolution(true);
+            System.out.println("You won the game!");
+        }
+
+    }
+
+    private void print() {
+        if (notFoundBombs != 0) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("Remaining bombs to find are " + notFoundBombs);
+
+            System.out.println();
         }
     }
 
